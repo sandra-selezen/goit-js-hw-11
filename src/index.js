b4bbbba6e9b868580d06b8a67608fdc2d36607ea
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-03-08 16:27:49 +0200

    Adds Notiflix

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+/*
+Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+Notify.info("We're sorry, but you've reached the end of search results.");
+Notify.success("Hooray! We found totalHits images.");
+*/
