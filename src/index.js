49a24043d09a0d4c01d760ca9c738cd76c72e1c0
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-03-09 21:44:15 +0200

    Adds styles for cards

--- src/index.js
+++ src/index.js
@@ -21,16 +21,16 @@ const renderGallery = items => {
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>${likes}
+      <b>Likes</b> ${likes}
     </p>
     <p class="info-item">
-      <b>Views</b>${views}
+      <b>Views</b> ${views}
     </p>
     <p class="info-item">
-      <b>Comments</b>${comments}
+      <b>Comments</b> ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>${downloads}
+      <b>Downloads</b> ${downloads}
     </p>
   </div>
 </div></a>`
